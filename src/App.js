ba7e6974d0c9e0a79bd385b5eeca03c1ea83c5b1
Robert Wilson <youmufan@gmail.com>
2018-04-03 18:49:13 -0400

    set up router and routes to each respective component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+
+//components
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+        </ul>
+        <Route path='/' exact component={Home} />
+        <Route path='/profile' exact component={Profile} />
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export const Profile = props => (
+    <div>This is our Profile!</div>
+)
